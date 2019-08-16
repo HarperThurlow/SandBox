dd51af122e352c22c6b0dd4c4141a11110a9451a
HarperT <harper.thurlow@my.jcu.edu.au>
2019-08-16 10:41:24 +0930

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,10 @@
 """Harper Thurlow"""
+
+MIN_LENGTH = 5
+
+password = input("Please enter a password longer than {} >".format(MIN_LENGTH))
+
+while len(password) <= MIN_LENGTH:
+    password = input("Please enter a password longer than {} >".format(MIN_LENGTH))
+
+print(len(password) * "*")
